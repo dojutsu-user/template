4576bac2fbddff68523c6903c3001f42c3bc02ba
dojutsu-user <vaibhgupt199@gmail.com>
2019-07-20 15:23:33 +0530

    test

--- docs/_static/js/custom.js
+++ docs/_static/js/custom.js
@@ -1,5 +1,7 @@
-function getInputField() {
-    console.log("THIS IS CUSTOM");
-    var custom_search_bar = document.querySelector('.my-custom-search-bar');
-    return custom_search_bar;
+READTHEDOCS = {
+    getInputField: function() {
+        console.log("THIS IS CUSTOM");
+        var custom_search_bar = document.querySelector('.my-custom-search-bar');
+        return custom_search_bar;
+    }
 }
