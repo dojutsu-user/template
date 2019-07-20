dc867a5776d40ac36f66fdba504d2f5905c1ec53
dojutsu-user <vaibhgupt199@gmail.com>
2019-07-20 15:42:52 +0530

    remove

--- docs/_static/js/custom.js
+++
@@ -1,7 +0,0 @@
-READTHEDOCS = {
-    getInputField: function() {
-        console.log("THIS IS CUSTOM");
-        var custom_search_bar = document.querySelector('.my-custom-search-bar');
-        return custom_search_bar;
-    }
-}
